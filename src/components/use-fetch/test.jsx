077ba358af288fc94a6032c6c3392f12610e821a
go2023 <tetyanago2018@gmail.com>
2024-02-20 19:10:43 -0800

    small fix

--- src/components/use-fetch/test.jsx
+++ src/components/use-fetch/test.jsx
@@ -6,12 +6,10 @@ const UseFetchHookTest = () => {
         {}
     );
 
-    console.log(data, error, loading);
-
     return (
         <div>
             <h1>Use FetchHook Test</h1>
-            {loading ? <h3>Loading ! Please wait</h3> : null}
+            {loading ? <h3>Loading...</h3> : null}
             {error ? <h3>{error}</h3> : null}
             {data && data.products && data.products.length
                 ? data.products.map((productItem, index) => (
